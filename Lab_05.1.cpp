761ee67034974f57dc4566bc28183c8d7c61a1ad
Roman <romanpridiba@gmail.com>
2023-10-30 20:12:29 +0200

    first commit

---
+++ Lab_05.1.cpp
@@ -0,0 +1,29 @@
+// Lab 5_1
+
+// ВАРІАНТ №21
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double g(const double a, const double b, const double c); // прототип
+
+
+
+int main()
+{
+	double x, y;
+	
+	cout << "x = "; cin >> x;
+	cout << "y = "; cin >> y;
+
+	double c = g(x * x, 1, y) - g(y * y, x, 1);
+	cout << "c = " << c << endl;
+	return 0;
+}
+
+double g(const double a, const double b, const double c) // визначення
+{
+	return (a * a + sin(b) + 1) / (1 + c * c);
+}
+
